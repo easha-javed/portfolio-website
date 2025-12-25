e182505c82666d93ff07440ec145d0e4d7ddf2dd
easha-javed <216410358+easha-javed@users.noreply.github.com>
2025-12-25 20:15:47 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
-
+export const metadata: Metadata = {
+  
+}
 export default function RootLayout({
   children,
 }: Readonly<{
